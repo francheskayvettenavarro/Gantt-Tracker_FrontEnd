9c54878679f9280f5efb22f6962e673f74e9c615
francheskayvettenavarro <78572896+francheskayvettenavarro@users.noreply.github.com>
2024-02-19 16:48:03 +0800

    file
    0.

---
+++ gantt-tracker-main/styles/global.js
@@ -0,0 +1,45 @@
+import css from 'styled-jsx/css';
+
+export default css.global`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  html,
+  body {
+    font-family: 'Montserrat', sans-serif;
+    color: #000;
+  }
+
+  input,
+  select {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 13px;
+    padding: 5px 7px;
+    margin: 1rem 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-weight: bold;
+  }
+
+  button {
+    cursor: pointer;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 15px;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  form {
+    padding: 1rem;
+  }
+
+  form > * {
+    display: flex;
+    align-items: center;
+  }
+`;
